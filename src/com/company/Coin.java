7e3555fd0a6d3b92517c64635938e664fb0abda3
Oriol Soler <ocusler@gmail.com>
2019-09-04 20:48:33 +0200

    coins definition

---
+++ src/com/company/Coin.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public enum Coin {
+    FIVE_CNT(0.05),
+    TEN_CNT(0.10),
+    TWENTY_CNT(0.20),
+    FIFTY_CNT(0.50),
+    EURO(1),
+    TWO_EURO(2);
+
+
+    public final double value;
+
+    Coin(double value) {
+        this.value = value;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import static com.company.Coin.EURO;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO JAVA, I MISSED U");
+        System.out.println(EURO.value);
+    }
+}
