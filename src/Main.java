22e08760c844b3f7dad789bf18179e88cc31161a
Oriol Soler <ocusler@gmail.com>
2019-09-05 17:15:32 +0200

    init values in test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
-import static domain.Coin.EURO;
-import static domain.Product.COKE;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(EURO.value);
-        System.out.println(COKE.price);
     }
 }
--- src/test/test.java
+++ src/test/test.java
@@ -18,33 +18,38 @@ public class test {
 
     VendingMachineController vendingMachineController = new VendingMachineController();
 
-    private List<Product> initialProducts = new ArrayList<Product>(Arrays.asList(COKE, SPRITE, WATER, COKE));
+    private List<Product> initialProducts = new ArrayList<Product>(Arrays.asList(
+            COKE, SPRITE, WATER,
+            COKE, SPRITE,
+            COKE));
+
     private List<Coin> initialCoins = new ArrayList<Coin>(Arrays.asList(
             FIVE_CNT, TEN_CNT, TWENTY_CNT, FIFTY_CNT, EURO, TWO_EURO,
-            FIVE_CNT, TEN_CNT, TWENTY_CNT, FIFTY_CNT, EURO, TWO_EURO,
-            FIVE_CNT, TEN_CNT, TWENTY_CNT, FIFTY_CNT, EURO, TWO_EURO,
-            FIVE_CNT, TEN_CNT, TWENTY_CNT, FIFTY_CNT, EURO, TWO_EURO,
-            FIVE_CNT, TEN_CNT, TWENTY_CNT, FIFTY_CNT, EURO, TWO_EURO));
+            FIVE_CNT, TEN_CNT, TWENTY_CNT, FIFTY_CNT, EURO,
+            FIVE_CNT, TEN_CNT, TWENTY_CNT, FIFTY_CNT,
+            FIVE_CNT, TEN_CNT, TWENTY_CNT,
+            FIVE_CNT, TEN_CNT,
+            FIVE_CNT));
 
     @Test
     public void coinsCorrectlyAdd() {
 
         vendingMachineController.refillCoins(initialCoins);
 
-        assertEquals(vendingMachineController.coinRemaining(FIFTY_CNT), 5);
+        assertEquals(vendingMachineController.coinRemaining(FIVE_CNT), 6);
         assertEquals(vendingMachineController.coinRemaining(TEN_CNT), 5);
-        assertEquals(vendingMachineController.coinRemaining(TWENTY_CNT), 5);
-        assertEquals(vendingMachineController.coinRemaining(FIFTY_CNT), 5);
-        assertEquals(vendingMachineController.coinRemaining(EURO), 5);
-        assertEquals(vendingMachineController.coinRemaining(TWO_EURO), 5);
+        assertEquals(vendingMachineController.coinRemaining(TWENTY_CNT), 4);
+        assertEquals(vendingMachineController.coinRemaining(FIFTY_CNT), 3);
+        assertEquals(vendingMachineController.coinRemaining(EURO), 2);
+        assertEquals(vendingMachineController.coinRemaining(TWO_EURO), 1);
     }
 
     @Test
     public void productsCorrectlyAdd() {
         vendingMachineController.refillProducts(initialProducts);
 
-        assertEquals(vendingMachineController.productRemaining(COKE), 2);
-        assertEquals(vendingMachineController.productRemaining(SPRITE), 1);
+        assertEquals(vendingMachineController.productRemaining(COKE), 3);
+        assertEquals(vendingMachineController.productRemaining(SPRITE), 2);
         assertEquals(vendingMachineController.productRemaining(WATER), 1);
     }
 }
